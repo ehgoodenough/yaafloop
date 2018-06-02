dbe1bb6ed28b69eecaaff0af78249d338abfb246
Andrew McPherson <ehgoodenough@gmail.com>
2018-06-02 10:19:05 -0700

    Switched to module exports.

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
 var Afloop = require("afloop")
 
-export default class Yaafloop {
-    constructor(func) {
-        this.func = func
+module.exports = function(func) {
+    this.func = func
 
-        this.fps = Yaafloop.fps || 60
-        this.cap = Yaafloop.cap || 1000
+    this.fps = 60
+    this.cap = 1000
 
-        this.loop = new Afloop((delta) => {
-            // Cap the delta.
-            if(typeof this.cap === "number") {
-                delta = Math.min(delta, this.cap)
-            }
+    return new Afloop((delta) => {
+        // Cap the delta.
+        if(typeof this.cap === "number") {
+            delta = Math.min(delta, this.cap)
+        }
 
-            // Calculate the delta in
-            // different units of time.
-            delta = {
-                ms: delta, // in milliseconds
-                s: delta / 1000, // in seconds
-                f: delta / (1000 / this.fps), // in frames
-            }
+        // Calculate the delta in
+        // different units of time.
+        delta = {
+            ms: delta, // in milliseconds
+            s: delta / 1000, // in seconds
+            f: delta / (1000 / this.fps), // in frames
+        }
 
-            // Call the function
-            // with the delta.
-            this.func(delta)
-        })
-    }
+        // Call the function
+        // with the delta.
+        this.func(delta)
+    })
 }
